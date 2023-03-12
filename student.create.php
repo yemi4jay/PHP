6f6cbf73c88d33f0e8833e92229d3b5a0bd44d39
yemi4jay <123545934+yemi4jay@users.noreply.github.com>
2023-03-12 01:42:32 +0000

    first commit

---
+++ student.create.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Justourist!</title>
+  </head>
+  <body>
+    <h1>Justourist!</h1>
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Justourist add
+                    <a href="index.php class="btn btn-danger float-end">BACK</a>
+                </h4>
+
+                
+            </div>
+            <div class="card-body">
+                <form action="" method= "POST">
+                    
+                    <div class="mb-3">
+                        <label>firstname surname</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>email</label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>phone</label>
+                        <input type="phone" name="phone" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>type of story</label>
+                        <input type="story" name="story" class="form-control">
+                    </div>
+
+
+
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+   
+  </body>
+</html>
